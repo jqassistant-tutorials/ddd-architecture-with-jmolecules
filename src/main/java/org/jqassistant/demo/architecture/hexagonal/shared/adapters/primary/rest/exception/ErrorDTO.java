02b1f445ac980c50bc22340109f0cb6398e5b7f6
Dirk Mahler <dirk.mahler@buschmais.com>
2026-02-18 14:21:32 +0100

    added baseline and suppression

--- src/main/java/org/jqassistant/demo/architecture/hexagonal/shared/adapters/primary/rest/exception/ErrorDTO.java
+++ src/main/java/org/jqassistant/demo/architecture/hexagonal/shared/adapters/primary/rest/exception/ErrorDTO.java
@@ -1,10 +1,13 @@
 package org.jqassistant.demo.architecture.hexagonal.shared.adapters.primary.rest.exception;
 
+import com.buschmais.jqassistant.plugin.java.annotation.jQASuppress;
+
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@jQASuppress(value = "rest:DTOMustBeLocatedInModelPackage", reason = "For demonstration purposes.", until = "2026-12-31")
 public class ErrorDTO {
 
     private String message;
